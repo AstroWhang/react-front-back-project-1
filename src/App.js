dc6f905e03dae87faf2b310b33b74fc42151f6fa
Astro Whang <astro.whang@bluevolt.com>
2019-07-19 17:02:37 -0700

    created .env.local file to store api id

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
     console.log(this.state.users);
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id="${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
 
     this.setState({
