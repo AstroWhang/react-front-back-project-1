e6e02af84c1d9613d1690a371b9433f3d16e0094
astrowhang <whangastro@gmail.com>
2019-07-21 12:45:45 -0700

    added clear search results button

--- src/App.js
+++ src/App.js
@@ -2,48 +2,63 @@ import React, { Component } from 'react';
 import Navbar from './component/layout/Navbar';
 // import UserItem from './component/users/UserItem';
 import User from './component/users/Users';
-import axios from 'axios';
+import Search from './component/users/Search';
 
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
-
-  // componentDidMount() {
-  //   axios.get('https://api.github.com/users').then(res => console.log(res.data))
-  // }
 
   state = {
     users: [],
     loading: false,
   };
 
-  async componentDidMount() {
+  // displayed a set of users when app first started, now blank before searching
+  // async componentDidMount() {
+  //   // can't change state directly need to use setState
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/users?client_id="${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({
+  //     users: res.data,
+  //     loading: false
+  //   });
+  // }
 
-    // can't change state directly need to use setState
-    this.setState({ loading: true });
-
-    console.log(this.state.users);
-
-    const res = await axios.get(`https://api.github.com/users?client_id="${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
+  // Search Github Users
+  // arrow function so put async before parameter rather than function
+  searchUsers = async (text) => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id="${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     });
+    console.log(res.data.items);
+  }
 
-    console.log(res.data);
+  // clear users from state
+
+  clearUsers = () => {
+    this.setState({
+      users: [], loading: false
+    });
   }
 
   render() {
+
+    const {loading, users} = this.state;
 
     return (
       <div className="App">
         <Navbar />
         <div className="container">
 
-          <User loading={this.state.loading} users={this.state.users} />
+
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+          <User loading={loading} users={users} />
         </div>
       </div>
     );
